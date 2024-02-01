2f7a7f2bc582357c4c25754910f8541c672a551a
Matthew Hope <matt@matthewhope.co.uk>
2024-02-01 21:58:21 +0000

    Remove redundant name field on ArtistVM

--- ui/ArtistViewModel.go
+++ ui/ArtistViewModel.go
@@ -6,3 +6,19 @@ type ArtistViewModel struct {
 	Data           models.Artist
 	DatesLocations map[string][]string
 }
+
+func (a ArtistViewModel) Prev() int {
+	res := (a.Data.ID - 1) % 52
+	if res == 0 {
+		res = 52
+	}
+	return res
+}
+
+func (a ArtistViewModel) Next() int {
+	res := (a.Data.ID + 1) % 52
+	if res == 0 {
+		res = 52
+	}
+	return res
+}
