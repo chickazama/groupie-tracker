ccc27be1d4a50a0a5fa1e1b647310320bebc4ae0
Matthew Hope <matt@matthewhope.co.uk>
2024-02-01 19:16:11 +0000

    Define Artists model

---
+++ models/Artist.go
@@ -0,0 +1,13 @@
+package models
+
+type Artist struct {
+	ID              int      `json:"id"`
+	Image           string   `json:"image"`
+	Name            string   `json:"name"`
+	Members         []string `json:"members"`
+	CreationDate    int      `json:"creationDate"`
+	FirstAlbum      string   `json:"firstAlbum"`
+	LocationsURL    string   `json:"locations"`
+	ConcertDatesURL string   `json:"concertDates"`
+	RelationsURL    string   `json:"relations"`
+}
