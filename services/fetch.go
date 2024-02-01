9f6eef6befcf77b57a269e47a3d955c408ab30a2
Matthew Hope <matt@matthewhope.co.uk>
2024-02-01 18:58:24 +0000

    Define basic fetch implementation

---
+++ services/fetch.go
@@ -0,0 +1,15 @@
+package services
+
+import "net/http"
+
+func fetch(url string) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
